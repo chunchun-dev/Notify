f26ee4efd6befe0f135fe7ac06e2dd31f7d409fb
qt-coder <chuchenkhang@gmail.com>
2021-05-05 19:58:13 +0800

    implemented auto redirect on auth

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,22 @@
 import NotePreviewGrid from '../components/NotePreviewGrid'
+import { useRouter } from 'next/router'
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home() {
-  return (
-    <>
-      <NotePreviewGrid/>
-    </>
-  )
+
+  const router = useRouter()
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  
+  if (isAuthenticated) {
+    return (
+      <>
+        <NotePreviewGrid/>
+      </>
+    )
+  } else {
+    router.push('/')
+
+    return <h1>bruh</h1>
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useRouter } from 'next/router'
+
 export default function Home() {
-  return (
-    <>
-      <h1>Landing page Page</h1>
-    </>
-  )
+
+
+  const router = useRouter()
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (!isLoading) {
+    if (isAuthenticated) {
+      router.push('/dashboard')
+      return <h1>Dashboard redirect</h1>
+    } else {
+      return (
+        <>
+          <h1>Landing page</h1>
+        </>
+      )
+    }
+  } else {
+    return 'loading'
+  }
 }
